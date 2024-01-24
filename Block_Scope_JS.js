47f9ca74ffdf070576bb6b0c0dc85028ff21a3de
PRATIK KUMAR SINGH <rishusingh3573@gmail.com>
2024-01-24 20:48:23 +0530

    commit
    global and block-accessible concept.
    Block-scope concept in JavaScript.

---
+++ Block_Scope_JS.js
@@ -0,0 +1,19 @@
+{
+    var a=10;
+    let b=20;
+    const c=30;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    
+}
+ console.log(a);
+    console.log(b);
+    console.log(c);
+    /* Block means enclosed within the curly brackets('{}'). 
+    And the scope means the accessible of the variable. 
+    Now the accessibel happens in two places i.e. one at the 1.Global area 
+    2. Block area.
+    In global area the variable is accessible globally whereas in local variable accessible
+    is only within the block . "let" and const have block accessible 
+    whereas var is globally accessible.
